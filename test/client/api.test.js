c3971162b8cddecece30476129063e3fce131bbb
Sarah Dueweke <sdueweke@gmail.com>
2017-06-01 11:31:44 +1200

    add basic test setup

---
+++ test/client/api.test.js
@@ -0,0 +1,17 @@
+import test from 'ava'
+import nock from 'nock'
+
+import * as api from '../../client/api'
+
+//
+// test.cb('api.getCategories', t => {
+//   let scope = nock('http://localhost:80')
+//     .get('/categories')
+//     .reply(200, {message: 'home page working'})
+//
+//   api.getCategories((actual) => {
+//     scope.done()
+//     t.is(actual.message, 'home page working')
+//     t.end()
+//   })
+// })
---
+++ test/client/index.test.js
@@ -0,0 +1,20 @@
+import './setup-dom'
+import test from 'ava'
+import React from 'react'
+import { shallow, mount } from 'enzyme'
+
+import App from '../../client/components/App'
+import * as api from '../../client/api'
+
+App.prototype.componentDidMount = () => {}
+
+// test('<App />', t => {
+//   const wrapper = shallow(<App />)
+//   t.is(wrapper.find('h1').text(),'Widgets FTW!')
+// })
+//
+// test.cb('mount <App />', t => {
+//   const wrapper = mount(<App />)
+//   t.is(wrapper.find('.widget-list').exists(),true)
+//   t.end()
+// })
