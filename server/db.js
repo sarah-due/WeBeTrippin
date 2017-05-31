b2c848ff263b96ae2fa094e66c26d795acf8798a
Sarah Dueweke <sdueweke@gmail.com>
2017-06-01 11:10:23 +1200

    set up files and temp data

---
+++ server/db.js
@@ -0,0 +1,7 @@
+/// Temp hard-coded data ///
+
+var countries = {
+  New Zealand: ['Auckland', 'Wellington', 'Christchurch', 'Dunedin', 'Queenstown'],
+  United Kingdom: ['London', 'Manchester', 'Liverpool', 'Cambridge' ],
+  United States: ['Washington D.C.', 'New York', 'Chicago', 'San Francisco', 'Miami']
+}
---
+++ server/routes/travel.js
@@ -0,0 +1,1 @@
+/// GET json from db
