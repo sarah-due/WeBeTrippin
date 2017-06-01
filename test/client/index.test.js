812edb750397e37923d89fb6b9877170c0d9358d
simonol1 <simondanolearyis@gmail.com>
2017-06-01 17:38:45 +1200

    added 4 passing tests

--- test/client/index.test.js
+++ test/client/index.test.js
@@ -4,16 +4,26 @@ import React from 'react'
 import { shallow, mount } from 'enzyme'
 
 import App from '../../client/components/App'
+import TravelForm from '../../client/components/TravelForm'
 
 App.prototype.componentDidMount = () => {}
 
 test('The home page shows our header', t => {
   const wrapper = shallow(<App />)
-  t.is(wrapper.find('.container').text(),'We Be Trippin')
+  t.is(wrapper.find('h1').text(),'We Be Trippin')
 })
-//
-// test.cb('mount <App />', t => {
-//   const wrapper = mount(<App />)
-//   t.is(wrapper.find('.widget-list').exists(),true)
-//   t.end()
-// })
+
+test('TravelForm renders on homepage', t => {
+  const wrapper = mount(<TravelForm />)
+  t.is(wrapper.find('label').exists(),true)
+})
+
+test('TravelForm label shows correct text', t => {
+  const wrapper = mount(<TravelForm />)
+  t.is(wrapper.find('label').text(),'Select Country')
+})
+
+test('TravelForm options render correctly on homepage', t => {
+  const wrapper = mount(<TravelForm />)
+  t.is(wrapper.find('option').exists(), true)
+})
