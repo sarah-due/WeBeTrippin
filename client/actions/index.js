afbf97be17eb18d0657c932389aeddc3c3e8aaba
simonol1 <simondanolearyis@gmail.com>
2017-06-02 12:08:45 +1200

    removed word action

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,12 +1,4 @@
-let nextWordId = 0
 
-export const addWord = (word) => {
-  return {
-    type: 'ADD_WORD',
-    id: nextWordId++,
-    word
-  }
-}
 
 export const fetchCities = (country) => {
   return {
