9770bc63c56f34309f71de2b0e1e4b42651f790a
simonol1 <simondanolearyis@gmail.com>
2017-06-02 13:27:43 +1200

    testing updates

--- test/server/travel.test.js
+++ test/server/travel.test.js
@@ -3,11 +3,9 @@ import request from 'supertest'
 
 import app from '../../server/server'
 
-
-
 test.serial.cb('GET /countries', t => {
   request(app)
-    .get('/api/countries')
+    .get('/')
     .expect(200)
     .end((err,res) => {
       t.deepEqual(Object.keys(res.body).length, 3)
